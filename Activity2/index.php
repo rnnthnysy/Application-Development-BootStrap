3ca123931f51038b81db10ac4c0e0bfcc7ce6be7
Ron Anthony Sy <142371703+rnnthnysy@users.noreply.github.com>
2024-03-22 15:44:56 +0800

    Add files via upload

---
+++ Activity2/index.php
@@ -0,0 +1,115 @@
+<html>
+   <head>
+        <title>3BSCS1</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<script src="js/jquery.js"></script>
+		<script src="js/bootstrap.js"></script>
+	
+	<title>Activity 2</title>
+	</head>
+	<body>
+	<h1>Activity 2</h1>
+	<a href="#">Messages<span class="badge">10<span></a><br/>
+<a href="#">Notification<span class="badge">30<span></a>
+<button class="btn btn-primary">Click to see notification<span class="badge">500</span></button>
+<div class="page-header">
+<h3>Download<span class="label label-default">Version 10.1</span></h3>
+</div>
+
+<div class="alert alert-danger fade in"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already Exist!!!</div>
+<div class="alert alert-success"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already Exist!!!</div>
+<div class="alert alert-warning"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already Exist!!!</div>
+<div class="alert alert-info"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already Exist!!!</div>
+
+<p class="well well-sm">This is the well</p>
+<p class="well">This is the well</p>
+<p class="well well-lg">This is the well</p>
+<br/>
+
+<spam class="glyphicon glyphicon-trash"></span>:Trash Can <br/>
+<spam class="glyphicon glyphicon-pencil"></span>:Pencil <br/>
+<button class="btn btn-success"><span class="glyphicon glyphicon-search"></span>Find</button>
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+<div class="dropdown">
+<button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">College<span class="caret"></span></button>
+<ul class="dropdown-menu">
+<li><a href="#">CCS</a></li>
+<li><a href="#">CET</a></li>
+<li><a href="#">CBA</a></li>
+<li class="divider"></li>
+<li class="dropdown-header">Exit</li>
+<li><a href="#">Logout</a></li>
+</ul>
+</div>
+
+<div class="dropup">
+<button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">College<span class="caret"></span></button>
+<ul class="dropdown-menu">
+<li><a href="#">CCS</a></li>
+<li><a href="#">CET</a></li>
+<li><a href="#">CBA</a></li>
+<li class="divider"></li>
+<li class="dropdown-header">Exit</li>
+<li><a href="#">Logout</a></li>
+</ul>
+</div>
+
+
+<button class="btn btn-default">Click Me</button>
+<button class="btn btn-primary btn-cs">Click Me</button>
+<button class="btn btn-info btn-lg">Click Me</button>
+<button class="btn btn-success btn-md">Click Me</button>
+<button class="btn btn-warning btn-sm">Click Me</button>
+<button class="btn btn-link">Click Me</button>
+<button class="btn btn-danger btn-block">Click Me</button>
+<button class="btn btn-danger disabled">Click Me</button>
+<button class="btn btn-danger active">Click Me</button>
+
+<div class="btn-group-vertical">
+<button class="btn btn-info">Home</button>
+<button class="btn btn-info">About Us</button>
+<button class="btn btn-info">Contact Us</button>
+<button class="btn btn-info">Products</button>
+</div>
+
+<div class="btn-group">
+<button class="btn btn-info">Home</button>
+<button class="btn btn-info">About Us</button>
+<button class="btn btn-info">Contact Us</button>
+<button class="btn btn-info">Products</button>
+</div>
+
+<div class="btn-group btn-group-justified">
+<div class="btn-group">
+<button class="btn btn-success">Home</button>
+</div>
+<div class="btn-group">
+<button class="btn btn-success">About Us</button>
+</div>
+<div class="btn-group">
+<button class="btn btn-success">Contact Us</button>
+</div>
+<div class="btn-group">
+<button class="btn btn-success">Products</button>
+</div>
+</div>
+
+<br/>
+
+<div class="container-fluid">
+	<img src="https://storage.googleapis.com/cdn.vcgamers.com/news/wp-content/uploads/2023/11/images-7-2.jpeg" class="img-rounded">
+	<img src="https://rare-gallery.com/thumbnail/1377025-sung-jinwoo-solo-leveling-manhwa-4k-pc-wallpaper.jpg" class="img-circle">
+    <img src="https://images.immediate.co.uk/production/volatile/sites/3/2024/01/Solo-Leveling-Crunchyroll-901cd60-e1704389620236.png?quality=90&webp=true&resize=1000,667" class="img-thumbnail">
+</div>		
+	
+		
+</body>
+  
+</html>
